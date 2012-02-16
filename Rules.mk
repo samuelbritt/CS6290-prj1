f0be60243794371e162a0fb42f50c6a37543da9e
Sam Britt <samuelbritt@gmail.com>
2012-02-16 00:02:55 -0500

    add debugging symbols

--- Rules.mk
+++ Rules.mk
@@ -1,6 +1,6 @@
 # Common variables to use in makefiles
 CC = gcc
-CFLAGS = -std=gnu99 -Wall -pedantic
+CFLAGS = -std=gnu99 -Wall -pedantic -g -O0
 
 LINK = $(LINK.c) -o $@ $^
 COMP = $(COMPILE.c) $(INCLUDES) $<
